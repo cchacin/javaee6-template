b99f42642b7ce6d904a31d137096cf499b09c4fb
Carlos Chacin <cchacin@groupon.com>
2014-06-06 17:07:50 -0400

    Refactor

--- src/main/java/com/example/javaee/endpoints/UserEndpoint.java
+++ src/main/java/com/example/javaee/endpoints/UserEndpoint.java
@@ -1,8 +1,8 @@
 package com.example.javaee.endpoints;
 
 import com.example.javaee.domain.User;
-import com.example.javaee.interceptors.ActionProtocolInterceptor;
-import com.example.javaee.interceptors.LogInterceptorBinding;
+import com.example.javaee.interceptors.Loggable;
+import com.example.javaee.interceptors.LoggingInterceptor;
 import com.example.javaee.repository.UserRepository;
 
 import javax.inject.Inject;
@@ -14,8 +14,8 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/users")
-@LogInterceptorBinding
-@Interceptors(ActionProtocolInterceptor.class)
+@Loggable
+@Interceptors(LoggingInterceptor.class)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserEndpoint {
 
