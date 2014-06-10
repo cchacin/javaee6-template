658782c95526b7168b1d35d5d2d6f4f8158cea42
Carlos Chacin <cchacin@groupon.com>
2014-06-10 00:12:45 -0400

    Logging new user events

---
+++ src/main/java/com/example/javaee/events/NewUserEvent.java
@@ -0,0 +1,17 @@
+package com.example.javaee.events;
+
+import com.example.javaee.entities.User;
+import com.example.javaee.qualifiers.New;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@Slf4j
+@ApplicationScoped
+public class NewUserEvent {
+
+    public void logNewUser(@Observes @New User newUser) {
+        log.info("New user created {}", newUser);
+    }
+}
