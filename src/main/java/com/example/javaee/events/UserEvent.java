d135e7726ccc6ab4cc661f1ac0cf51eb82e72e98
Carlos Chacin <cchacin@groupon.com>
2014-06-11 14:10:38 -0400

    Events refactored

--- src/main/java/com/example/javaee/events/UserEvent.java
+++ src/main/java/com/example/javaee/events/UserEvent.java
@@ -1,6 +1,7 @@
 package com.example.javaee.events;
 
 import com.example.javaee.entities.User;
+import com.example.javaee.qualifiers.Edited;
 import com.example.javaee.qualifiers.New;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -9,12 +10,16 @@ import javax.inject.Inject;
 import java.util.logging.Logger;
 
 @ApplicationScoped
-public class NewUserEvent {
+public class UserEvent {
 
 	@Inject
 	private transient Logger logger;
 
 	public void logNewUser(@Observes @New User newUser) {
 		logger.info("New user created " + newUser);
+	}
+
+	public void logEditedUser(@Observes @Edited User editedUser) {
+		logger.info("User edited " + editedUser);
 	}
 }
