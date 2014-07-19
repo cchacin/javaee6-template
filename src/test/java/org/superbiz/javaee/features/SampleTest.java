83f9c885df1764e6271290f14b3893f51f76a890
Carlos Chacin <cchacin@groupon.com>
2014-07-19 01:57:16 -0400

    gh-pages

---
+++ src/test/java/org/superbiz/javaee/features/SampleTest.java
@@ -0,0 +1,13 @@
+package org.superbiz.javaee.features;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
