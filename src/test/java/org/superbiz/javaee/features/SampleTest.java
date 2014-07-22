7e2e084cf6633ebd672893342ed470af042e5e05
Carlos Chacin <cchacin@groupon.com>
2014-07-22 04:35:58 -0400

    Test

--- src/test/java/org/superbiz/javaee/features/SampleTest.java
+++ src/test/java/org/superbiz/javaee/features/SampleTest.java
@@ -1,13 +1,27 @@
 package org.superbiz.javaee.features;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.superbiz.javaee.entities.dtos.UserDTO;
+
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SampleTest {
 
+	private UserDTO cut;
+
+	@Before
+	public void init() {
+		this.cut = new UserDTO("cchacin@superbiz.org", "Carlos", "passw0rd");
+		this.cut.setId(1L);
+		this.cut.setVersion(0L);
+		this.cut.setCreated(new Date(12341234));
+	}
+
 	@Test
 	public void sampleTest() {
-		assertThat(1 + 1).isEqualTo(2);
+		assertThat(this.cut.getEmail()).isEqualTo("cchacin@superbiz.org");
 	}
 }
