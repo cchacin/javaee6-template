2a002d19d994499354f2127ef61ca2c5557dd3da
Carlos Chacin <cchacin@groupon.com>
2014-08-02 20:36:33 -0400

    Fixes #6 -> ServletContextListener to shutdown/unregister drivers:
    Reference: http://bit.ly/1sfaycb

---
+++ src/main/java/org/superbiz/javaee/listeners/AppContextListener.java
@@ -0,0 +1,65 @@
+package org.superbiz.javaee.listeners;
+
+import com.mysql.jdbc.AbandonedConnectionCleanupThread;
+import org.redisson.Redisson;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.util.Enumeration;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+	@Inject
+	private transient Logger logger;
+
+	@Inject
+	private transient Redisson redisson;
+
+	@Override
+	public void contextInitialized(final ServletContextEvent servletContextEvent) {
+		logger.info("Initializing application context: {}", servletContextEvent
+				.getServletContext().getContextPath());
+	}
+
+	@Override
+	public void contextDestroyed(final ServletContextEvent servletContextEvent) {
+		logger.info("Destroying application context: {}", servletContextEvent
+				.getServletContext().getContextPath());
+		try {
+			logger.info("Shutting down AbandonedConnection");
+			AbandonedConnectionCleanupThread.shutdown();
+		} catch (Throwable t) {
+			logger.error("Exception shutting down AbandonedConnection: {}", t);
+		}
+
+		// This manually deregisters JDBC driver, which prevents Tomcat 7 from
+		// complaining about memory leaks
+		Enumeration<Driver> drivers = java.sql.DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			java.sql.Driver driver = drivers.nextElement();
+			logger.info("Deregistering JDBC driver: {}", driver);
+			try {
+				java.sql.DriverManager.deregisterDriver(driver);
+			} catch (Throwable t) {
+				logger.error("Exception deregistering drivers: {}", t);
+			}
+		}
+		try {
+			Thread.sleep(2000L);
+		} catch (Exception e) {
+			logger.error("Fatal error: {}", e);
+		}
+
+		try {
+			logger.info("Shutting down Redisson: {}", redisson.getConfig());
+			// redisson.shutdown();
+		} catch (Exception e) {
+			logger.error("Error shutting down Redisson: {}", e);
+		}
+	}
+}
